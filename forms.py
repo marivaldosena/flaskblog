9dc05226a4149a9a2f8040b2f7cf8f9303df8d68
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-13 09:58:30 -0300

    Criar formulário de login

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -16,3 +16,14 @@ class RegistrationForm(FlaskForm):
               validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail',
+              validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
