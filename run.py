68481e7aa304760164b19ea75d2d3698b33a07bb
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-13 10:56:37 -0300

    Incluir validação de formulário e flash messages.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 posts = [
@@ -31,7 +33,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
-    
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('form.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('form.html', title='Log in', form=form)
 
 if __name__ == '__main__':
     app.run()
