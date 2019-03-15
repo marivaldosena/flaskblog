d865ddbea8ade63ceeb0bcf848efc0f8b0302f13
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-15 20:00:51 -0300

    Incluir funcionalidade para salvar imagens e trocá-las no formulário de atualização de imagem de perfil.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -19,7 +20,7 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 from models import User, Post
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, UpdateAccountForm
 
 @app.route('/')
 @app.route('/home')
@@ -81,7 +82,35 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+
+    if form.validate_on_submit():
+        if  form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+    else:
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account Info',
+                        image_file=image_file, form=form)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static', 'profile_pics', picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
--- forms.py
+++ forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, \
     ValidationError
 from models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -38,8 +40,36 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
 
     password = PasswordField('Password',
-                             validators=[DataRequired(), Length(min=6)])
+                        validators=[DataRequired(), Length(min=6)])
 
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                        validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('E-mail',
+                        validators=[DataRequired(), Email()])
+    
+    picture = FileField('Profile Picture',
+                        validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, field):
+        if field.data != current_user.username:
+            user = User.query.filter_by(username=field.data).first()
+        
+            if user:
+                raise ValidationError('Username is already taken.')
+
+    def validate_email(self, field):
+        if field.data != current_user.email:
+            user = User.query.filter_by(email=field.data).first()
+
+            if user:
+                raise ValidationError('E-mail is already taken.')
+
