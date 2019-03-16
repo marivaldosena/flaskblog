4a811fe28354f69d4f20550f866f76b3572e06c4
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-15 23:41:35 -0300

    Ordenar posts pelos mais recentes.

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ from forms import RegistrationForm, LoginForm, UpdateAccountForm, \
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('index.html', posts=posts)
 
 
