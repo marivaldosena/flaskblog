ab855df40b8b3537ea224fa07c3e374d0c5743b3
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-15 23:47:31 -0300

    Obter posts por usuário.

--- app.py
+++ app.py
@@ -182,3 +182,13 @@ def save_picture(form_picture):
     i.save(picture_path)
 
     return picture_fn
+
+
+@app.route('/users/<username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)
+            .order_by(Post.date_posted.desc()) \
+            .paginate(page=page, per_page=5)
+    return render_template('posts/show.html', posts=posts)
